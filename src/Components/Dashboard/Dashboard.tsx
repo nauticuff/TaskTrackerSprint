76fa43af1d74a95f1ce015ae5e0eeaab9f252abf
Pedro Castaneda <petecast76@gmail.com>
2023-04-25 12:00:07 -0700

    Moved type/react from dependencies to devDep.

---
+++ src/Components/Dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import './Dashboard.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const DashboardComponent = (): JSX.Element => {
+    return (
+        <>
+            <p>hi</p>
+        </>
+    )
+}
+
+export default DashboardComponent;
