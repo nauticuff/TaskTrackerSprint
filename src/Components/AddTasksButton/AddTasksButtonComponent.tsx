3998048062d1f9f2b34a8ef8aa9c97f0c4fb4c34
Pedro Castaneda <petecast76@gmail.com>
2023-04-27 11:55:30 -0700

    Started dropdown for tasks(...)

--- src/Components/AddTasksButton/AddTasksButtonComponent.tsx
+++ src/Components/AddTasksButton/AddTasksButtonComponent.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
+import { Button, Form, Row, Col } from 'react-bootstrap';
 import Modal from 'react-bootstrap/Modal';
 import PlusSign from '../../Assets/plus.svg';
 
@@ -12,20 +12,46 @@ const AddTasksButton = (): JSX.Element => {
   return (
     <>
       <Button className='bg-transparent border border-0 border-transparent my-0' onClick={handleShow}>
-        <img className=''  style={{ height: '2.2rem' }} src={PlusSign} alt="add icon" />
+        <img className='' style={{ height: '2.2rem' }} src={PlusSign} alt="add icon" />
       </Button>
 
       <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+        <Modal.Header className='bg-dark' closeButton>
+          <Modal.Title className='text-light'>Add Task</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
+        <Modal.Body className='bg-dark text-light'>
+          <Form>
+            <Row className="mb-3">
+              <Form.Group as={Col} className='col-12' controlId="formTitle">
+                <Form.Label>Title</Form.Label>
+                <Form.Control type="text" placeholder="Enter title" />
+              </Form.Group>
+
+              <Form.Group as={Col} className='col-12' controlId="formDescription">
+                <Form.Label>Description</Form.Label>
+                <Form.Control type="textarea"  placeholder="Description" />
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridState">
+                <Form.Label>State</Form.Label>
+                <Form.Select defaultValue="Choose...">
+                  <option>Choose...</option>
+                  <option>...</option>
+                </Form.Select>
+              </Form.Group>
+            </Row>
+            {/* <Button variant="primary" type="submit">
+        Submit
+      </Button> */}
+          </Form>
+        </Modal.Body>
+        <Modal.Footer className='bg-dark text-light'>
+          <Button onClick={handleClose}>
+            Cancel
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
+          <Button onClick={handleClose}>
+            Add
           </Button>
         </Modal.Footer>
       </Modal>
