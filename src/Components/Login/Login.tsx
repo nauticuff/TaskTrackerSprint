f95c1d10527cc2a4c6961466e02347c30e0004fd
JLamzon <jlamzon@yahoo.com>
2023-04-26 19:40:00 -0700

    add font, change input to form

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Form, Row, Col, Button } from "react-bootstrap";
 import NotesPic from "../../Assets/NotesPic.jpg"
 import "../Login/Login.css"
 import handwavepic from "../../Assets/handwavepic.png"
 import NotePic from "../../Assets/notePic.png"
 
 function Login() {
+
     return (
         <div className="loginScreen">
             <div className="">
-
                 <Row className="loginMobile">
                     <Col md={5} className="loginContainer">
                         <div className="titleAndLogin">
                             <div className="titleAndLogo">
-                                <img className="notePicImage" src={NotePic}/>
+                                <img className="notePicImage" src={NotePic} />
                                 <h1 className="titleText text-center">Task Tracker</h1>
                             </div>
                             <div className="loginWindow">
@@ -23,10 +23,18 @@ function Login() {
                                     <img className="handPic" src={handwavepic} />
                                 </div>
                                 <p className="introText">Login to continue developing amazing ideas</p>
-                                <p className="">Username</p>
-                                <input />
-                                <p className="passTextSpace">Password</p>
-                                <input />
+                                <Form className="enterLogin">
+                                    <Form.Group>
+                                        <label className="">Username</label>
+                                        <input />
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <label className="passTextSpace">Password</label>
+                                        <input type="password" />
+                                    </Form.Group>
+                                </Form>
+
+
                                 <Button>Login</Button>
                                 <div className="newAndCreateText">
                                     <p className="newText">New User? <span className="createText">Create Account</span></p>
@@ -35,7 +43,7 @@ function Login() {
                         </div>
                     </Col>
                     <Col md={7} className="imageCol">
-                        <p className="logoText">Get more done quickly,<br/>launch products sooner.</p>
+                        <p className="logoText">Get more done quickly,<br />launch products sooner.</p>
                         <img className="loginImage" src={NotesPic} />
                     </Col>
                 </Row>
